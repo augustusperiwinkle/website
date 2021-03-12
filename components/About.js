23f143ba9f97601337c84150cdb994c4d2cea14a
Derek Louis <thedereklouis@gmail.com>
2021-03-12 09:04:45 -0500

    added updated resume

--- components/About.js
+++ components/About.js
@@ -143,11 +143,7 @@ export class About extends React.Component {
                 id="heroku"
               />
             </div>
-            <a
-              href="../Derek_Louis_Resume.docx.pdf"
-              target="_blank"
-              id="resume"
-            >
+            <a href="../Derek_Louis_Resume.pdf" target="_blank" id="resume">
               <i className="fas fa-file-pdf" id="pdfLogo"></i>View Resume
             </a>
           </div>
