47bc3a71178c323ff88894619035e769825f0f64
Derek Louis <thedereklouis@gmail.com>
2021-05-14 12:10:40 -0400

    added correct font to credit. Updated about description.

--- components/About.js
+++ components/About.js
@@ -36,11 +36,11 @@ export default class About extends React.Component {
             <div id="textAndImage">
               <img src="./photos/codePicSmall.jpg" id="myPhoto" />
               <div className="aboutPageText">
-                Derek Louis is a passionate software developer with a
-                certificate in fullstack software engineering from Fullstack
-                Academy. After traveling the world for 10 years as a tour
-                manager and professional musician, Derek brings a high level of
-                leadership, adaptability and creativity to any dev team.
+                Derek Louis is a passionate software engineer with a certificate
+                in fullstack software engineering from Fullstack Academy. After
+                traveling the world for 10 years as a tour manager and
+                professional musician, Derek brings a high level of leadership,
+                adaptability and creativity to any dev team.
                 <br />
                 <br />
                 When Derek isn't coding, he's most likely learning about or
