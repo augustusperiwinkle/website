2945b6069f9f7071bc40e368846e5095327594a7
Derek Louis <thedereklouis@gmail.com>
2021-05-15 20:32:56 -0400

    added landing animation

--- components/Routes.js
+++ components/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import Animation from './Animation';
 import WorkPage from './WorkPage';
 import Contact from './Contact';
 import About from './About';
@@ -12,8 +13,10 @@ export default class Routes extends React.Component {
     super();
   }
   render() {
+    const animationStatus = localStorage.getItem('animationPlayed');
     return (
       <>
+        {animationStatus !== 'true' && <Animation />}
         <Switch>
           <Redirect exact from="/" to="/work" />
           <Route path="/work" component={WorkPage} />
