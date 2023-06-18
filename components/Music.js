9e8a86173b8d35339e72cd5998bba630de971d94
Derek Louis <thedereklouis@gmail.com>
2023-06-18 19:40:50 -0400

    photos sourced from imgur

--- components/Music.js
+++ components/Music.js
@@ -7,20 +7,22 @@ import musicData from '../musicData';
 function Music(props) {
   return (
     <div className="page-wrapper">
-      <div id="master-credit">
-        All songs written and performed by Derek Louis unless otherwise
-        specified
-      </div>
       <div id="music-wrapper">
         {musicData.map((song) => {
           return (
             <div className="song-wrapper">
               <div className="left-column">
                 <div className="song-name">{song.songName}</div>
-                <img src={`./albumArt/${song.albumArtPathname}.jpg`} />
+                <img
+                  src={song.albumArtPathname}
+                  alt={`${song.songName} Artwork`}
+                />
               </div>
               <div className="right-column">
                 <div className="credits-wrapper">
+                  <div className="master-credit">
+                    Music & Lyrics by Derek Louis
+                  </div>
                   {song.credits.map((credit) => {
                     return (
                       <div className="credit">
@@ -43,6 +45,20 @@ function Music(props) {
                     className="song-link-anchor"
                   >
                     <i className="fab fa-apple song-link-icon"></i>
+                  </a>
+                  <a
+                    href={song.youTubeLink}
+                    target="_blank"
+                    className="song-link-anchor"
+                  >
+                    <i className="fab fa-youtube song-link-icon"></i>
+                  </a>
+                  <a
+                    href={song.soundCloudLink}
+                    target="_blank"
+                    className="song-link-anchor"
+                  >
+                    <i className="fab fa-soundcloud song-link-icon"></i>
                   </a>
                   <a
                     href={song.amazonMusicLink}
--- musicData.js
+++ musicData.js
@@ -1,15 +1,30 @@
+const musicDataTemplate = {
+  songName: '',
+  albumArtPathname: '',
+  releaseDate: '',
+  spotifyLink: '',
+  appleMusicLink: '',
+  youTubeLink: '',
+  soundCloudLink: '',
+  amazonMusicLink: '',
+  credits: [['', '']],
+};
+
 const musicData = [
   {
     songName: 'Whittle My Life',
-    albumArtPathname: 'whittleMyLife',
+    albumArtPathname: 'https://i.imgur.com/ode4AVy.jpg',
     releaseDate: '6/7/23',
     spotifyLink:
       'https://open.spotify.com/track/1a85qkKcp8whrdYNM1uLCS?si=e70bcde10ea64264',
     appleMusicLink:
       'https://music.apple.com/us/album/whittle-my-life/1689522730?i=1689522731',
+    youTubeLink: '',
+    soundCloudLink: '',
     amazonMusicLink:
       'https://music.amazon.com/albums/B0C68XK5K6?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_c5zI3JaLHWRpcbPDiJzLFtVJO&trackAsin=B0C693V3W5',
     credits: [
+      ['Derek Louis', 'Vocals, Guitar, Synth, Drum Programming'],
       ['Robin Buyer', 'Mixing Engineer'],
       ['Ryan Schwabe', 'Mastering Engineer'],
       ['Dana Kenney', 'Artwork'],
@@ -17,12 +32,15 @@ const musicData = [
   },
   {
     songName: 'Hey! My Youth Is Back',
-    albumArtPathname: 'heyMyYouthIsBack',
+    albumArtPathname: 'https://i.imgur.com/YWUVHlH.jpg',
     releaseDate: '',
     spotifyLink: '',
     appleMusicLink: '',
+    youTubeLink: '',
+    soundCloudLink: '',
     amazonMusicLink: '',
     credits: [
+      ['Derek Louis', 'Vocals, Drum Set, Percussion, Guitar, Synth'],
       ['Eddie Torres', 'Percussion'],
       ['John Cushing', 'Trombone'],
       ['Tree Palmedo', 'Trumpet'],
