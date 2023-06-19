91694e5e0b638d4a1d01ee632b44bdd43a58069a
Derek Louis <thedereklouis@gmail.com>
2023-06-19 10:40:30 -0400

    responsive music page

--- musicData.js
+++ musicData.js
@@ -12,25 +12,6 @@ const musicDataTemplate = {
 
 const musicData = [
   {
-    songName: 'Whittle My Life',
-    albumArtPathname: 'https://i.imgur.com/ode4AVy.jpg',
-    releaseDate: '6/7/23',
-    spotifyLink:
-      'https://open.spotify.com/track/1a85qkKcp8whrdYNM1uLCS?si=e70bcde10ea64264',
-    appleMusicLink:
-      'https://music.apple.com/us/album/whittle-my-life/1689522730?i=1689522731',
-    youTubeLink: '',
-    soundCloudLink: '',
-    amazonMusicLink:
-      'https://music.amazon.com/albums/B0C68XK5K6?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_c5zI3JaLHWRpcbPDiJzLFtVJO&trackAsin=B0C693V3W5',
-    credits: [
-      ['Derek Louis', 'Vocals, Guitar, Synth, Drum Programming'],
-      ['Robin Buyer', 'Mixing Engineer'],
-      ['Ryan Schwabe', 'Mastering Engineer'],
-      ['Dana Kenney', 'Artwork'],
-    ],
-  },
-  {
     songName: 'Hey! My Youth Is Back',
     albumArtPathname: 'https://i.imgur.com/YWUVHlH.jpg',
     releaseDate: '',
@@ -47,6 +28,25 @@ const musicData = [
       ['Robin Buyer', 'Mixing Engineer'],
       ['Ryan Schwabe', 'Mastering Engineer'],
       ['Erica Erica', 'Artwork'],
+    ],
+  },
+  {
+    songName: 'Whittle My Life',
+    albumArtPathname: 'https://i.imgur.com/ode4AVy.jpg',
+    releaseDate: '6/7/23',
+    spotifyLink:
+      'https://open.spotify.com/track/1a85qkKcp8whrdYNM1uLCS?si=e70bcde10ea64264',
+    appleMusicLink:
+      'https://music.apple.com/us/album/whittle-my-life/1689522730?i=1689522731',
+    youTubeLink: '',
+    soundCloudLink: '',
+    amazonMusicLink:
+      'https://music.amazon.com/albums/B0C68XK5K6?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_c5zI3JaLHWRpcbPDiJzLFtVJO&trackAsin=B0C693V3W5',
+    credits: [
+      ['Derek Louis', 'Vocals, Guitar, Synth, Drum Programming'],
+      ['Robin Buyer', 'Mixing Engineer'],
+      ['Ryan Schwabe', 'Mastering Engineer'],
+      ['Dana Kenney', 'Artwork'],
     ],
   },
 ];
